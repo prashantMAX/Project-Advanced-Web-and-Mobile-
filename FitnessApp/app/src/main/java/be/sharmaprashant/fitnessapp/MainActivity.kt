6d8b6fca4de133fee2f879cef657772bbcaacc49
Gaurav Ghimire <gaurav.ghimire@student.odisee.be>
2024-05-13 18:51:28 +0200

    Update

--- FitnessApp/app/src/main/java/be/sharmaprashant/fitnessapp/MainActivity.kt
+++ FitnessApp/app/src/main/java/be/sharmaprashant/fitnessapp/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FitnessAppTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    FitnessApp()
+                        FitnessApp()
                     }
                 }
             }
