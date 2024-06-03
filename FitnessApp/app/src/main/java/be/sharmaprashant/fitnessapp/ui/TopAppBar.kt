6c682a0775164de38bafccf6b6279849b8914606
Gaurav Ghimire <gaurav.ghimire@student.odisee.be>
2024-06-03 15:25:01 +0200

    Update

--- FitnessApp/app/src/main/java/be/sharmaprashant/fitnessapp/ui/TopAppBar.kt
+++ FitnessApp/app/src/main/java/be/sharmaprashant/fitnessapp/ui/TopAppBar.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
